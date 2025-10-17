16dac9101226acc06126ba5a5d7c74718ebc30bc
BORDA_Elorien <elorien.borda@gmail.com>
2025-10-17 15:33:06 +0200

    avancement interface

--- interface_graphique.py
+++ interface_graphique.py
@@ -15,11 +15,11 @@ class App(tk.Tk):
         ecran = tk.Frame(self, bg="#121213")
         ecran.pack(side="top", fill="x", pady=20, padx=20)
         
-        display_current_formula = tk.Label(ecran, text="0", bg="#3a3a3c", anchor="w", justify="left", font=("Arial", 30), fg="white")
-        display_current_formula.pack(fill="x")
+        self.display_current_formula = tk.Label(ecran, text="0", bg="#5e5e5e", anchor="w", justify="left", font=("Arial", 30), fg="white")
+        self.display_current_formula.pack(fill="x")
         
-        display_result = tk.Label(ecran, text="", bg="#3a3a3c", justify="left", anchor="w", font=("Arial", 15), fg="white")
-        display_result.pack(fill="x")
+        self.display_result = tk.Label(ecran, text="", bg="#5e5e5e", justify="left", anchor="w", font=("Arial", 20), fg="white")
+        self.display_result.pack(fill="x")
 
         #boutons
         buttons = tk.Frame(self, bg="#121213")
@@ -38,14 +38,37 @@ class App(tk.Tk):
         
         for i in range(4):
             for j in range(4):
-                button = tk.Button(buttons, bg="#3a3a3c", fg="white", text=button_names[i][j], font=("Arial", 30), width=5)
+                button = tk.Button(buttons, text=button_names[i][j], 
+                                   command = lambda name=button_names[i][j]: self.button_pressed(name),
+                                   bg="#3a3a3c", fg="white", font=("Arial", 30), width=5)
                 button.grid(row=i, column=j, padx=5, pady=5)
         for j in range(0, 4, 2):
-            button = tk.Button(buttons, bg="#53794e", fg="white", text=button_names[4][j//2], font=("Arial", 30), width=5)
+            button = tk.Button(buttons, text=button_names[4][j//2],
+                               command = lambda name=button_names[4][j//2]: self.button_pressed(name),
+                               bg="#53794e", fg="white", font=("Arial", 30), width=5)
             button.grid(row=4, column=j, columnspan=2, padx=5, pady=5, sticky="nswe")
 
 
-        current_formula = "0"
+        self.current_formula = "0"
+
+    
+    def button_pressed(self, name):
+        if name == "=":
+            self.display_result.configure(text=str(self.calculeResultat(self.current_formula)))
+        
+        else:           
+            if name == "AC":
+                self.current_formula = "0"
+            elif self.current_formula == "0":
+                self.current_formula = name
+            else:
+                self.current_formula += name
+            self.display_current_formula.configure(text=self.current_formula)
+
+
+    def calculeResultat(self, formula):
+        return "0"
+
 
 app = App()
 app.mainloop()
